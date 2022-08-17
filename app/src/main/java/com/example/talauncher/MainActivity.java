2a74deae1812f481b0d0741eb834f9e7c6d95065
theamit10 <prasadak66@gmail.com>
2022-08-17 12:44:09 +0530

    changes in adapter

--- app/src/main/java/com/example/talauncher/MainActivity.java
+++ app/src/main/java/com/example/talauncher/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.talauncher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.GridView;
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         initializeDrawer();
     }
 
-    List<AppObject> appList = new ArrayList<>();
+    List<AppObject> installedAppList = new ArrayList<>();
 
     private void initializeDrawer() {
         View mBottomSheet = findViewById(R.id.bottomSheet);
@@ -33,11 +36,35 @@ public class MainActivity extends AppCompatActivity {
 
 //        mBottomSheetBehavior.setHideable(false);
 //        mBottomSheetBehavior.setPeekHeight(300);
+//
+        // adding manually
+//        for(int i=0 ;i<20; i++){
+//            appList.add(new AppObject(String.valueOf(i),"",getDrawable(R.drawable.ic_launcher_foreground)));
+//        }
 
-        for(int i=0 ;i<20; i++){
-            appList.add(new AppObject(String.valueOf(i),"",getDrawable(R.drawable.ic_launcher_foreground)));
+        installedAppList = getInstalledAppList();
+
+        mDrawerGridView.setAdapter(new AppAdapter(getApplicationContext(), installedAppList));
+    }
+
+    private List<AppObject> getInstalledAppList() {
+        List<AppObject> list = new ArrayList<>();
+        Intent intent = new Intent(Intent.ACTION_MAIN,null);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> untreatedAppList = getApplicationContext().getPackageManager().queryIntentActivities(intent,0);
+
+        for(ResolveInfo untreatedApp : untreatedAppList){
+            String appName = untreatedApp.activityInfo.loadLabel(getPackageManager()).toString();
+            String appPackageName = untreatedApp.activityInfo.packageName;
+            Drawable appImage = untreatedApp.activityInfo.loadIcon(getPackageManager());
+
+            AppObject app = new AppObject(appName,appPackageName,appImage);
+            if(!list.contains(app)){
+                list.add(app);
+            }
+
         }
 
-        mDrawerGridView.setAdapter(new AppAdapter(getApplicationContext(), appList));
+        return list;
     }
 }
--- app/src/main/java/com/example/talauncher/adapter/AppAdapter.java
+++ app/src/main/java/com/example/talauncher/adapter/AppAdapter.java
@@ -1,11 +1,13 @@
 package com.example.talauncher.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
@@ -38,23 +40,36 @@ public class AppAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(final int i, View view, ViewGroup parent) {
 
         View v;
         if(view == null){
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-            v = inflater.inflate(R.layout.item_app,viewGroup, false);
+            v = inflater.inflate(R.layout.item_app,parent, false);
 
         }else {
             v = view;
         }
 
 
+        LinearLayout mlayout = v.findViewById(R.id.layout);
         ImageView mimage = v.findViewById(R.id.image);
         TextView mlabel = v.findViewById(R.id.label);
 
         mimage.setImageDrawable(appList.get(i).getImage());
         mlabel.setText(appList.get(i).getName());
+
+        mlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent launchAppIntent = context.getPackageManager().getLaunchIntentForPackage(appList.get(i).getPackageName());
+
+                if(launchAppIntent != null){
+                    context.startActivity(launchAppIntent);
+                }
+
+            }
+        });
 
 
         return v;
