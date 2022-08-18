108c8fb744d1ea0d9d1a3781a2e05a4b54d89c7e
theamit10 <prasadak66@gmail.com>
2022-08-18 11:44:43 +0530

    created home sccreen pagerObject and viewPagerAdapter

--- app/src/main/java/com/example/talauncher/MainActivity.java
+++ app/src/main/java/com/example/talauncher/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.talauncher;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
@@ -10,7 +12,9 @@ import android.view.View;
 import android.widget.GridView;
 
 import com.example.talauncher.adapter.AppAdapter;
+import com.example.talauncher.adapter.viewPagerAdapter;
 import com.example.talauncher.model.AppObject;
+import com.example.talauncher.model.PagerObject;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.util.ArrayList;
@@ -18,12 +22,33 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean isbottom = true;
+    ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initializeDrawer();
+        initializeHome();
+    }
+
+    private void initializeHome() {
+        ArrayList<PagerObject> pagerAppList = new ArrayList<>();
+        ArrayList<AppObject> appList =  new ArrayList<>();
+
+        for(int i=0;i<20;i++){
+            appList.add(new AppObject("","",getResources().getDrawable(R.drawable.ic_launcher_foreground)));
+
+        }
+
+        pagerAppList.add(new PagerObject(appList));
+        pagerAppList.add(new PagerObject(appList));
+        pagerAppList.add(new PagerObject(appList));
+
+        mViewPager = findViewById(R.id.viewPager);
+        mViewPager.setAdapter(new viewPagerAdapter(this, pagerAppList));
     }
 
     List<AppObject> installedAppList = new ArrayList<>();
@@ -32,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         View mBottomSheet = findViewById(R.id.bottomSheet);
         final GridView mDrawerGridView = findViewById(R.id.drawerGrid);
 
-        BottomSheetBehavior mBottomSheetBehavior = BottomSheetBehavior.from(mBottomSheet);
+        final BottomSheetBehavior mBottomSheetBehavior = BottomSheetBehavior.from(mBottomSheet);
 
 //        mBottomSheetBehavior.setHideable(false);
 //        mBottomSheetBehavior.setPeekHeight(300);
@@ -45,6 +70,24 @@ public class MainActivity extends AppCompatActivity {
         installedAppList = getInstalledAppList();
 
         mDrawerGridView.setAdapter(new AppAdapter(getApplicationContext(), installedAppList));
+
+        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if(newState == BottomSheetBehavior.STATE_HIDDEN && mDrawerGridView.getChildAt(0).getY() != 0){
+                    mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }
+                if(newState == BottomSheetBehavior.STATE_DRAGGING && mDrawerGridView.getChildAt(0).getY() != 0){
+                    mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
     }
 
     private List<AppObject> getInstalledAppList() {
---
+++ app/src/main/java/com/example/talauncher/adapter/viewPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.example.talauncher.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.talauncher.R;
+import com.example.talauncher.model.PagerObject;
+
+import java.util.ArrayList;
+
+public class viewPagerAdapter extends PagerAdapter {
+
+    Context context;
+    ArrayList<PagerObject> pagerAppList;
+
+    public viewPagerAdapter(Context context, ArrayList<PagerObject> pagerAppList) {
+        this.context = context;
+        this.pagerAppList = pagerAppList;
+    }
+
+    // this is responsible for to show the item in the app
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.pager_layout, container, false);
+
+        final GridView mGridView = layout.findViewById(R.id.grid);
+        mGridView.setAdapter(new AppAdapter(context,pagerAppList.get(position).getAppList()));
+
+        container.addView(layout);
+
+        return layout;
+    }
+
+    // due to this only after getting all the home screen page also our app will not crash
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return pagerAppList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+}
